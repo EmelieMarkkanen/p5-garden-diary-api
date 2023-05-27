c3643a86647fc4801cd1321c99514d5dc6c7087b
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-05-27 18:27:53 +0000

    Add tasks model, serializer, urls and views, migrated to db
    Url also added to garden_diary urls

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
+from garden_diary.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class TaskListView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Task.objects.filter(owner=user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
