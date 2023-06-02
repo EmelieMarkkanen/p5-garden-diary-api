8da1665ed24115f7ac40d294bf5d2ce470964c3d
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-02 14:13:41 +0000

    Set profiles model back to default image, no fix for overwriting image, backpedal to try and fix later

--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
-
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,7 +9,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/' #default='../default_profile_c36wy0'
+        upload_to='images/', default='../default_profile_c36wy0'
     )
 
     class Meta:
