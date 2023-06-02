15beb71f6e03511a6316554373d8e8d3b7d1bd6c
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-02 17:36:51 +0000

    Update plants views for filtering of posts

--- plants/views.py
+++ plants/views.py
@@ -7,29 +7,21 @@ from rest_framework.response import Response
 
 
 class PlantsListView(generics.ListCreateAPIView):
-    queryset = Plants.objects.all()
     serializer_class = PlantsSerializer
     permission_classes = [IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Plants.objects.filter(owner=user)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class PlantsDetailedView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Plants.objects.all()
     serializer_class = PlantsDetailSerializer
     permission_classes = [IsOwnerOrReadOnly]
 
-    def get(self, request, *args, **kwargs):
-        pk = self.kwargs['pk']
-        queryset = self.get_queryset()
-        obj = get_object_or_404(queryset, pk=pk)
-        serializer = self.get_serializer(obj)
-        return Response(serializer.data)
-
     def get_queryset(self):
-        if self.request.user.is_authenticated:
-            user = self.request.user
-            return Plants.objects.filter(owner=user)
-        else:
-            return Plants.objects.all()
+        user = self.request.user
+        return Plants.objects.filter(owner=user)
