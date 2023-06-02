7aaea0155e5f906ce5219b8120658d39aff983e8
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-02 13:03:36 +0000

    Update profile model to delete default image when user uploads their own

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
+from django.core.files import File
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -11,9 +13,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     country = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default='../default_profile_c36wy0'
-    )
+    image = models.ImageField(upload_to='images/', default='../default_profile_c36wy0')
 
     class Meta:
         ordering = ['-created_at']
@@ -22,14 +22,14 @@ class Profile(models.Model):
         return f"{self.owner}'s profile"
 
 
+@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile.objects.create(owner=instance)
         if instance.profileImage:
+            profile.image.delete()
             profile.image.save(
                 instance.profileImage.name,
-                ContentFile(instance.profileImage.read()),
+                instance.profileImage,
             )
         profile.save()
-
-post_save.connect(create_profile, sender=User)
