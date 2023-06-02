402f28c836e9e758530eb9fcbff6b3dbe69c2ac3
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-02 13:44:18 +0000

    Remove default image to possibly solve user profile image bug

--- profiles/models.py
+++ profiles/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
-from django.core.files import File
-from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -12,24 +10,23 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_profile_c36wy0')
-
+    image = models.ImageField(
+        upload_to='images/'
+    )
     class Meta:
         ordering = ['-created_at']
-
     def __str__(self):
         return f"{self.owner}'s profile"
+    
 
-
-@receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile.objects.create(owner=instance)
         if instance.profileImage:
-            if profile.image.name != '../default_profile_c36wy0': 
-                profile.image.delete()
             profile.image.save(
                 instance.profileImage.name,
-                instance.profileImage,
+                ContentFile(instance.profileImage.read()),
             )
-        profile.save()
+    profile.save()
+
+post_save.connect(create_profile, sender=User)
