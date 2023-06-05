5178b464f8935c592223cf804bf6f38b1af32324
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-05 16:47:10 +0000

    Update plant model and serializer, remove redundant fields and update planted_at field name

--- plants/models.py
+++ plants/models.py
@@ -3,18 +3,10 @@ from django.contrib.auth.models import User
 
 class Plants(models.Model):
 
-    YES = 'Yes'
-    NO = 'No'
-    YES_NO_CHOICES = [
-        (True, YES),
-        (False, NO),
-    ]
-
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
-    planted = models.DateField(null=True, blank=True)
-    perennial = models.BooleanField(default=False, choices=YES_NO_CHOICES)
+    planted_at = models.DateField(null=True, blank=True)
     care_instructions = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
