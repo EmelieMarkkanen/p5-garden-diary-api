5c096d1134733b1f792944e116106176a65b4934
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-05 15:53:24 +0000

    Add post_count, followers_count, following_count to profile serializer

--- followers/serializers.py
+++ followers/serializers.py
@@ -13,9 +13,30 @@ class FollowerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follower
-        fields = [
-            'id', 'owner', 'created_at', 'followed', 'followed_name'
-        ]
+        from django.db import IntegrityError
+from rest_framework import serializers
+from .models import Follower
+
+
+class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method handles the unique constraint on 'owner' and 'followed'
+    """
+    owner = serializers.ReadOnlyField(source='owner.username')
+    followed_name = serializers.ReadOnlyField(source='followed.username')
+
+    class Meta:
+        model = Follower
+        fields = '__all__'
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
 
     def create(self, validated_data):
         try:
