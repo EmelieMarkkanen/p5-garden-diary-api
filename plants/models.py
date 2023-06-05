acae1fae2cfd9fa287b6b61832a840b3b620fd1b
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-05 17:02:51 +0000

    Update plants model and serializer

--- plants/models.py
+++ plants/models.py
@@ -2,10 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Plants(models.Model):
+    
+    plant_type_choices = [
+        ('unknown', 'Unknown'),
+        ('berry_bush', 'Berry bush'),
+        ('herb', 'Herb'),
+        ('bulb', 'Bulb'),
+        ('cacti', 'Cacti'),
+        ('fern', 'Fern'),
+        ('grass', 'Grass'),
+        ('shrub', 'Shrub'),
+        ('tree', 'Tree'),
+        ('annual', 'Annual'),
+        ('biennial', 'Biennial'),
+        ('perennial', 'Perennial'),
+        ('vegetable', 'Vegetable'),
+        ('fruit_tree', 'Fruit tree'),
+        ('succulent', 'Succulent'),
+    ]
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    type = models.CharField(max_length=255)
+    plant_type = models.CharField(max_length=255, choices=plant_type_choices, null=True, blank=True, default='')
     planted_at = models.DateField(null=True, blank=True)
     care_instructions = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
--- plants/serializers.py
+++ plants/serializers.py
@@ -4,10 +4,10 @@ from .models import Plants
 class PlantsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plants
-        fields = ['id', 'name', 'type', 'planted_at', 'care_instructions', 'created_at', 'updated_at', 'image']
+        fields = ['id', 'name', 'plant_type', 'planted_at', 'care_instructions', 'created_at', 'updated_at', 'image']
 
 class PlantsDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Plants
-        fields = ['id', 'name', 'type', 'planted_at', 'care_instructions', 'created_at', 'updated_at', 'image']
+        fields = ['id', 'name', 'plant_type', 'planted_at', 'care_instructions', 'created_at', 'updated_at', 'image']
