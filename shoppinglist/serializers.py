278e7e05fb24763779e5c894a69dc229e37b3653
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-08 12:42:47 +0000

    Update shoppinglist serializer return

--- shoppinglist/serializers.py
+++ shoppinglist/serializers.py
@@ -1,20 +1,18 @@
-from rest_framework import serializers
-from .models import ShoppingList, ShoppingListItem
-
 class ShoppingListItemSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = ShoppingListItem
-        fields = ['id', 'shopping_list', 'owner', 'name', 'quantity']
-        
+        fields = ['id', 'name', 'quantity', 'owner']
+
 
 class ShoppingListSerializer(serializers.ModelSerializer):
-    items = ShoppingListItemSerializer(many=True, read_only=True)
+    items = ShoppingListItemSerializer(many=True, read_only=True, source='items')
     items_count = serializers.SerializerMethodField()
 
-    def get_items_count(self, obj):
-        return obj.items.count()
+    def get_items(self, obj):
+        items = obj.items.all()
+        return [{"id": item.id, "name": item.name} for item in items]
 
     class Meta:
         model = ShoppingList
