2b5ba9c8edf07ff87e953440916ca236fcc05e9e
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-09 17:41:41 +0000

    Remove unneccessary code from shoppinglist serializer

--- shoppinglist/serializers.py
+++ shoppinglist/serializers.py
@@ -3,19 +3,13 @@ from .models import Item
 
 class ItemSerializer(serializers.ModelSerializer):
     """Serializer for the Item model."""
-    item = serializers.CharField(source='name')
-    quantity = serializers.IntegerField()
-
     class Meta:
         model = Item
-        fields = ['item', 'quantity']
+        fields = ['name', 'quantity']
 
 
 class ItemDetailSerializer(serializers.ModelSerializer):
     """Serializer for the detailed Item model."""
-    item = serializers.CharField(source='name')
-    quantity = serializers.IntegerField()
-
     class Meta:
         model = Item
         fields = ['item', 'quantity', 'owner', 'created_at', 'updated_at']
