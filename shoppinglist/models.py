7a1f790f440c597fa93325297b292b9d037f3d16
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-09 16:31:52 +0000

    Migrate Item

--- shoppinglist/models.py
+++ shoppinglist/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Item(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    quantity = models.PositiveIntegerField(null=True)
+    quantity = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
