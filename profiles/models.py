60a542dc005183374f961ebc5256d0a976ce988a
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-12 12:17:08 +0000

    Remove default profile image

--- profiles/models.py
+++ profiles/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_c36wy0'
+        upload_to='images/' #default='../default_profile_c36wy0'
     )
 
     class Meta:
