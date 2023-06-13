9d0b39cc7ab8d892416ba404d84788a64dfd93ee
Emelie Markkanen Åberg <emeliemarkkanen@hotmail.com>
2023-06-13 16:12:17 +0000

    Add tests for plants model and views

---
+++ plants/test_models.py
@@ -0,0 +1,30 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from datetime import date
+from .models import Plants
+
+class PlantsModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user = User.objects.create_user(username='testuser', password='testpass')
+        Plants.objects.create(
+            owner=user,
+            name='Rose',
+            plant_type='shrub',
+            planted_at=date.today(),
+            care_instructions='Water regularly'
+        )
+
+    def test_name_max_length(self):
+        plant = Plants.objects.get(id=1)
+        max_length = plant._meta.get_field('name').max_length
+        self.assertEquals(max_length, 255)
+
+    def test_ordering(self):
+        plants = Plants.objects.all()
+        expected_ordering = ['-created_at']
+        self.assertListEqual(plants.model._meta.ordering, expected_ordering)
+
+    def test_string_representation(self):
+        plant = Plants.objects.get(id=1)
+        self.assertEqual(str(plant), '1 Rose')
---
+++ plants/tests_views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+from .models import Plants
+
+class PlantsListViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client.force_authenticate(user=self.user)
+
+    def test_plants_list(self):
+        response = self.client.get('/plants/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_plant(self):
+        data = {'name': 'Rose', 'care_instructions': 'Some instructions'}
+        response = self.client.post('/plants/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+
+class PlantsDetailedViewTest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.plant = Plants.objects.create(name='Rose', care_instructions='Some instructions', owner=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_plant(self):
+        response = self.client.get(f'/plants/{self.plant.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_plant(self):
+        data = {'name': 'Sunflower', 'care_instructions': 'Some other instructions'}
+        response = self.client.put(f'/plants/{self.plant.id}/', data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_plant(self):
+        response = self.client.delete(f'/plants/{self.plant.id}/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
